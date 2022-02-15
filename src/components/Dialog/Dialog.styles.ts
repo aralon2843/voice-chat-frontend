3b70f75d368efe59993d6d76751dbef71a461f86
aralon2843 <aralon2843@gmail.com>
2022-02-16 00:33:33 +0300

    feat(dialog): [WIP] added some css, extended message interface

--- src/components/Dialog/Dialog.styles.ts
+++ src/components/Dialog/Dialog.styles.ts
@@ -1,51 +1,69 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const DialogWrapper = styled.div`
-	border-left: 2px solid ${({ theme }) => theme.colors.semiDarkGrey};
-	flex: 1 1 65%;
-	background-color: ${({ theme }) => theme.colors.grey};
-	display: flex;
-	flex-direction: column;
-	padding: 0 20px;
-	height: 80vh;
-	position: relative;
-	height: 100%;
-	overflow-y: auto;
+  border-left: 2px solid ${({ theme }) => theme.colors.semiDarkGrey};
+  flex: 1 1 65%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.grey};
+  padding: 0 20px;
+  height: 80vh;
+  position: relative;
+  height: 100%;
+  overflow-y: auto;
+`;
+
+export const Messages = styled.div`
+  flex: 0 0 90%;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const Message = styled.div<{ isMe: boolean }>`
-	display: inline-block;
-	margin-bottom: 15px;
-	text-align: ${({ isMe }) => (isMe ? 'right' : 'left')};
+  display: inline-block;
+  margin-bottom: 15px;
+  text-align: ${({ isMe }) => (isMe ? "right" : "left")};
+  background-color: ${({ theme, isMe }) =>
+    isMe ? theme.colors.semiLightGrey : theme.colors.semiDarkGrey};
+  border-radius: 15px;
+  padding: 15px 10px;
+  width: 100%;
+  position: relative;
+`;
+
+export const MessageDate = styled.span<{ isMe: boolean }>`
+  border-radius: 15px;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.colors.lightBlue};
+  padding: 2px 4px;
+  position: absolute;
+  bottom: 7px;
+  ${({ isMe }) => (isMe ? "left: 7px" : "right: 7px")};
 `;
 
 export const MessageAvatar = styled.img`
-	height: 40px;
-	width: 40px;
-	border-radius: 50%;
-	object-fit: cover;
-	object-position: center;
-	margin-bottom: 5px;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: center;
+  margin-bottom: 5px;
 `;
 
 export const MessageText = styled.p<{ isMe: boolean }>`
-	color: ${({ theme }) => theme.colors.white};
-	text-align: ${({ isMe }) => (isMe ? 'right' : 'left')};
+  color: ${({ theme }) => theme.colors.white};
+  text-align: ${({ isMe }) => (isMe ? "right" : "left")};
 `;
 
-
 export const MessageTextArea = styled.textarea`
-	width: 90%;
-	resize: none;
-	height: 60px;
-	outline: none;
-	border: none;
-	border-radius: 15px;
-	position: absolute;
-	bottom: 0px;
-	left: calc(50% + 10px);
-	transform: translateX(-50%);
-	padding: 10px 15px;
-	color: ${({ theme }) => theme.colors.white};
-	background-color: ${({ theme }) => theme.colors.semiDarkGrey};
+  width: 90%;
+  resize: none;
+  height: 60px;
+  outline: none;
+  border: none;
+  border-radius: 15px;
+  padding: 10px 15px;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.semiDarkGrey};
 `;
--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,20 +1,19 @@
-import {
-  ChangeEvent,
-  ChangeEventHandler,
-  EventHandler,
-  memo,
-  useEffect,
-  useState,
-} from "react";
+import { ChangeEvent, memo, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { io, Socket } from "socket.io-client";
+import { useAppSelector } from "../../hooks/redux";
 import { IUser } from "../../models/IUser";
+import { FlexContainer } from "../App/App.styles";
+import { SendPostIcon } from "../Profile/Profile.styles";
 import {
   DialogWrapper,
   Message,
+  MessageDate,
   MessageText,
   MessageTextArea,
+  Messages,
 } from "./Dialog.styles";
-
+import sendIcon from "../../assets/icons/send.svg";
 interface IDialogProps {
   me: IUser;
   dialogId: string;
@@ -22,24 +21,24 @@ interface IDialogProps {
 
 interface IMessage {
   sender: string;
-  dialog: string;
+  dialogId: string;
   body: string;
+  createdAt: Date;
 }
 
 const Dialog: React.FC<IDialogProps> = ({ me, dialogId }): JSX.Element => {
-  const chatSocket: Socket = io("http://localhost:3001/chat");
-  // const alertSocket: Socket = io('http://localhost:3001/alerts');
-
+  const chatSocket: Socket = io("http://localhost:3001/message");
+  const { _id: meId } = useAppSelector((state) => state.userReducer.me);
   const [messageValue, setMessageValue] = useState("");
 
   const [messages, setMessages] = useState<IMessage[]>([]);
-  // const [alerts, setAlerts] = useState<string[]>([]);
+  const params = useParams();
 
   const onSendMessage = (message: string): void => {
     if (message.trim() !== "") {
       chatSocket.emit("messageToServer", {
-        sender: me.username,
-        dialog: dialogId,
+        sender: me._id,
+        dialogId: dialogId,
         body: message,
       });
       setMessageValue("");
@@ -69,39 +68,39 @@ const Dialog: React.FC<IDialogProps> = ({ me, dialogId }): JSX.Element => {
 
   return (
     <DialogWrapper>
-      {messages.map((message) => {
-        // return (
-        //   message.dialogId === params.dialogId && (
-        //     <Message isMe={meId === message.authorId}>
-        //       <MessageAvatar src={message.authorAvatarUrl} />
-        //       <MessageText isMe={meId === message.authorId}>
-        //         {message.body}
-        //       </MessageText>
-        //     </Message>
-        //   )
-        // );
-        return (
-          <Message isMe={false}>
-            <MessageText isMe={false}>
-              {message.sender}: {message.body}
-            </MessageText>
-          </Message>
-        );
-      })}
-      <MessageTextArea
-        placeholder="text message..."
-        value={messageValue}
-        onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-          setMessageValue(e.target.value);
-        }}
-      />
-      <button
-        onClick={() => {
-          onSendMessage(messageValue);
-        }}
-      >
-        Send message
-      </button>
+      <Messages>
+        {messages.map((message: IMessage) => {
+          return (
+            message.dialogId === params.dialogId && (
+              <Message isMe={meId === message.sender}>
+                {/* <MessageAvatar src={message.authorAvatarUrl} /> */}
+                <MessageDate isMe={meId === message.sender}>
+                  {new Date(message.createdAt).toLocaleTimeString()}
+                </MessageDate>
+                <MessageText isMe={meId === message.sender}>
+                  {message.body}
+                </MessageText>
+              </Message>
+            )
+          );
+        })}
+      </Messages>
+      <FlexContainer justify="space-between" align="center">
+        <MessageTextArea
+          placeholder="text message..."
+          value={messageValue}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+            setMessageValue(e.target.value);
+          }}
+        />
+        <SendPostIcon
+          src={sendIcon}
+          alt="send"
+          onClick={() => {
+            onSendMessage(messageValue);
+          }}
+        />
+      </FlexContainer>
     </DialogWrapper>
   );
 };
